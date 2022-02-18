e51f81c30eaf7df84e87f20c2c63293eec26f72e
laquiante <33266194+laquiante@users.noreply.github.com>
2022-02-18 11:02:43 +0100

    Update step-02.sh

--- step-02.sh
+++ step-02.sh
@@ -1,2 +1,3 @@
 # Simple EVPN setup L2 host11:host22 and L3 host11:extrouter22
-sudo ansible-playbook -i /home/ubuntu/ON-17/inventory/files/hosts ./step-02/main.yaml
+# ref 18-FEB-2022
+sudo ansible-playbook -i /home/ubuntu/ON-17/inventory/files/hosts ./step-0y/main.yaml
