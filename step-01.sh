d13dbac2eeae30c4a2133c9221adf9784ae526df
laquiante <33266194+laquiante@users.noreply.github.com>
2022-02-18 10:55:33 +0100

    Update and rename step-01.sh to step-0x.sh

--- step-01.sh
+++
@@ -1,1 +0,0 @@
-sudo ansible-playbook -i /home/ubuntu/ON-17/inventory/files/hosts ./step-01/main.yaml
---
+++ step-0x.sh
@@ -0,0 +1,1 @@
+sudo ansible-playbook -i /home/ubuntu/ON-17/inventory/files/hosts ./step-0x/main.yaml
